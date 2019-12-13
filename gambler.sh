300ffc5d56bf27be74636eb07ed768d72edbdc8c
kajal764 <kajaldw666@gmail.com>
2019-12-13 15:45:00 +0530

    Added: Day Limit For Win And Loss

--- gambler.sh
+++ gambler.sh
@@ -6,9 +6,19 @@ echo "!!! Welcome To Gambling Simulator... !!!"
 BET=1
 STAKE=100
 
-if [[ $((RANDOM%2)) -eq 0 ]]
-then
-	echo "win"
-else
-	echo "loss"
-fi
+#variable
+limit=$((50*$STAKE/100))
+lowLimit=$(($STAKE-$limit))
+highLimit=$(($STAKE+$limit))
+cash=$STAKE
+
+while [[ $cash -gt $lowLimit && $cash -lt $highLimit ]]
+do
+	if [[ $((RANDOM%2)) -eq 0 ]]
+	then
+		cash=$(($cash+$BET))
+	else
+		cash=$(($cash-$BET))
+	fi
+done
+echo $cash
