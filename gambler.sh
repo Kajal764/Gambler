0a9bb0f2ac707a3e53668753b12560d19ba01828
kajal764 <kajaldw666@gmail.com>
2019-12-13 16:52:24 +0530

    Added:Calculate Win ,Loss And Cash Value For Each Day

--- gambler.sh
+++ gambler.sh
@@ -5,20 +5,26 @@ echo "!!! Welcome To Gambling Simulator... !!!"
 #constant
 BET=1
 STAKE=100
+LIMIT=$(( 50 * $STAKE / 100 ))
+LOW_LIMIT=$(( STAKE - LIMIT))
+HIGH_LIMIT=$((STAKE + LIMIT))
 
-#variable
-limit=$((50*$STAKE/100))
-lowLimit=$(($STAKE-$limit))
-highLimit=$(($STAKE+$limit))
-cash=$STAKE
 
-while [[ $cash -gt $lowLimit && $cash -lt $highLimit ]]
+for (( i=1;i<=20;i++ ))
 do
-	if [[ $((RANDOM%2)) -eq 0 ]]
-	then
-		cash=$(($cash+$BET))
-	else
-		cash=$(($cash-$BET))
-	fi
+	cash=$STAKE
+	win=0
+	loss=0
+	while [[ $cash -gt $LOW_LIMIT && $cash -lt $HIGH_LIMIT ]]
+	do
+		if [[ $((RANDOM%2)) -eq 0 ]]
+		then
+			win=$(($win+1))
+			cash=$(($cash+$BET))
+		else
+			loss=$(($loss+1))
+			cash=$(($cash-$BET))
+		fi
+	done
+	echo $cash	$win	$loss
 done
-echo $cash
